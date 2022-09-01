8374dfb0384323cbaf64cb297e9466927c78ade1
keencho <seyoung050412@gmail.com>
2022-09-01 14:35:10 +0900

    update for jitpack publish

--- src/main/java/com/keencho/lib/spring/security/repository/KcAccountRepository.java
+++ src/main/java/com/keencho/lib/spring/security/repository/KcAccountRepository.java
@@ -2,16 +2,14 @@ package com.keencho.lib.spring.security.repository;
 
 import com.keencho.lib.spring.security.model.KcAccountBaseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * 계정 기본 레포지토리
  *
  * 비즈니스 로직에 따라 메소드 추가하여 사용해도 될듯.
- * @param <T>
- * @param <ID>
+ * @param <T> entity extends KcAccountBase
+ * @param <ID> entity id
  */
-@Repository
 public interface KcAccountRepository<T extends KcAccountBaseModel, ID> extends JpaRepository<T, ID> {
     T findByLoginId(String loginId);
 }
--- src/main/java/com/keencho/lib/spring/security/resolver/KcAccountResolver.java
+++ src/main/java/com/keencho/lib/spring/security/resolver/KcAccountResolver.java
@@ -1,7 +1,8 @@
 package com.keencho.lib.spring.security.resolver;
 
+import com.keencho.lib.spring.security.model.KcAccountBaseModel;
 import com.keencho.lib.spring.security.model.KcSecurityAccount;
 
-public interface KcAccountResolver<T> {
+public interface KcAccountResolver<T extends KcAccountBaseModel> {
     T getAccountBySecurityAccount(KcSecurityAccount securityAccount);
 }
