053fd1bf3ca7c19bad86d0e59f76e2b733630f49
keencho <seyoung050412@gmail.com>
2023-01-11 09:43:28 +0900

    update - configuration javax -> jakarta test

--- src/main/java/com/keencho/lib/spring/jpa/querydsl/KcQuerydslAnnotationProcessor.java
+++ src/main/java/com/keencho/lib/spring/jpa/querydsl/KcQuerydslAnnotationProcessor.java
@@ -4,9 +4,11 @@ import com.keencho.lib.spring.jpa.querydsl.annotation.KcQueryProjection;
 import com.querydsl.apt.Configuration;
 import com.querydsl.apt.ExtendedTypeFactory;
 import com.querydsl.apt.jpa.JPAAnnotationProcessor;
+import com.querydsl.apt.jpa.JPAConfiguration;
 import com.querydsl.codegen.EntityType;
 import com.querydsl.codegen.Property;
 import com.querydsl.codegen.utils.model.TypeCategory;
+import jakarta.persistence.*;
 
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -14,6 +16,7 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.ElementFilter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.Set;
 
@@ -25,6 +28,17 @@ public class KcQuerydslAnnotationProcessor extends JPAAnnotationProcessor {
     private ExtendedTypeFactory typeFactory;
 
     @Override
+    protected Configuration createConfiguration(RoundEnvironment roundEnv) {
+        Class<? extends Annotation> entity = Entity.class;
+        Class<? extends Annotation> superType = MappedSuperclass.class;
+        Class<? extends Annotation> embeddable = Embeddable.class;
+        Class<? extends Annotation> embedded = Embedded.class;
+        Class<? extends Annotation> skip = Transient.class;
+        return new JPAConfiguration(roundEnv, processingEnv,
+                entity, superType, embeddable, embedded, skip);
+    }
+
+    @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (roundEnv.processingOver()) {
             return JPAAnnotationProcessor.ALLOW_OTHER_PROCESSORS_TO_CLAIM_ANNOTATIONS;
@@ -33,7 +47,7 @@ public class KcQuerydslAnnotationProcessor extends JPAAnnotationProcessor {
         var result = super.process(annotations, roundEnv);
 
         this.roundEnv = roundEnv;
-        this.conf = super.createConfiguration(this.roundEnv);
+        this.conf = this.createConfiguration(this.roundEnv);
         this.typeFactory = new ExtendedTypeFactory(processingEnv, conf.getEntityAnnotations(), conf.getTypeMappings(), conf.getQueryTypeFactory(), conf.getVariableNameFunction());
         this.generateAndSerialize();
 
